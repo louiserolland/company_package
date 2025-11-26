428a2b6db7742fcae4f12a0a422b2257ce93bc3a
louiserolland <louiserolland04@gmail.com>
2025-11-26 19:00:19 +0000

    added Dockerfile

--- company/version.py
+++ company/version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.post2'
-__version_tuple__ = version_tuple = (0, 0, 'post2')
+__version__ = version = '0.0.post3'
+__version_tuple__ = version_tuple = (0, 0, 'post3')
 
-__commit_id__ = commit_id = 'ge0a27eef5'
+__commit_id__ = commit_id = 'gf6d405fd6'
